50c7b749933599cad202c13eacaf90e06cdbeeb0
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-03-16 22:00:17 +0100

    Add pages and router

---
+++ src/pages/Product/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Product = () => {
+  return <main>Product</main>;
+};
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export * from "./Home/index.jsx";
+export * from "./Product/index.jsx";
+export * from "./Checkout/index.jsx";
+export * from "./Cart/index.jsx";
+export * from "./Contact/index.jsx";
+export * from "./404/index.jsx";
