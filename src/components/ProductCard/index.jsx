fc94bf3e56cf4b128d09108cf15be3db8b2dc7f0
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-03-22 14:58:35 +0100

    Create product card component

---
+++ src/components/ProductCard/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { AverageRating } from "../AverageRating";
+import { PrimaryButton } from "../Button/index.styles";
+import { ProductDiscount, ProductPrice } from "../ProductPrice";
+import * as S from "./index.styles";
+
+function ProductCard({
+  product: {
+    imageUrl,
+    title,
+
+    price,
+    discountedPrice,
+    reviews,
+    id,
+  },
+}) {
+  return (
+    <S.ProductWrapper>
+      <S.ProductImage src={imageUrl} alt={title} />
+      <ProductDiscount discountedPrice={discountedPrice} price={price} />
+      <S.CardBody>
+        <S.ProductHeading>{title}</S.ProductHeading>
+        <AverageRating reviews={reviews} />
+        <S.CardFooter>
+          <Link to={`/Product/${id}`}>
+            {" "}
+            <PrimaryButton>View</PrimaryButton>
+          </Link>
+          <ProductPrice discountedPrice={discountedPrice} price={price} />
+        </S.CardFooter>
+      </S.CardBody>
+    </S.ProductWrapper>
+  );
+}
+
+export default ProductCard;
---
+++ src/components/ProductCard/index.styles.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const ProductWrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  box-shadow: rgba(33, 35, 38, 0.1) 0px 10px 10px -10px;
+  height: fit-content;
+  position: relative;
+`;
+
+export const CardBody = styled.div`
+  background-color: white;
+  border-radius: 0 0 10px 10px;
+  padding: 1rem;
+`;
+
+export const ProductImage = styled.img`
+  width: 100%;
+  height: 280px;
+  object-fit: cover;
+  border-radius: 10px 10px 0 0;
+`;
+
+export const ProductHeading = styled.h3`
+  font-size: 18px;
+  font-weight: 700;
+  margin-top: 0.4rem;
+`;
+
+export const CardFooter = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 1rem;
+`;
