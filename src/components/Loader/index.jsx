cd4899088fde1c8255c023900021bdf1b2268a81
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-03-26 16:41:48 +0200

    Create loader component

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./index.styles.js";
+
+const Loader = () => {
+  return (
+    <S.LoaderWrapper>
+      <S.Loader></S.Loader>
+    </S.LoaderWrapper>
+  );
+};
+
+export default Loader;
