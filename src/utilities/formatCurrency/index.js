db3816cbd9f63597a883a67617c49580edbeb3a4
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-03-26 16:40:31 +0200

    Create reusable currency formatter

---
+++ src/utilities/formatCurrency/index.js
@@ -0,0 +1,16 @@
+const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
+  currency: "NOK",
+  style: "currency",
+});
+
+/**
+ * Formats a number as a currency string using the Norwegian Krone (NOK) currency format.
+ * @param {number} number - The number to format as currency.
+ * @returns {string} string - The formatted currency string.
+ * @example
+ * const price = 1234.56;
+ * const formattedPrice = formatCurrency(price); // "kr 1 234,56"
+ */
+export function formatCurrency(number) {
+  return CURRENCY_FORMATTER.format(number);
+}
