7272ac2efafa09a83fd31b80fd526afe9293e0d1
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-03-27 22:40:26 +0200

    Updated cart icon component

---
+++ src/components/CartIcon/index.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import { useCart } from "../../hooks/useCart/index.jsx";
+import { CgShoppingBag } from "react-icons/cg";
+import * as S from "./index.styles.js";
+
+/**
+ * CartIcon component displays the shopping cart icon with the total number of items in the cart
+ * @returns {JSX.Element} Returns JSX Element of CartIcon component
+ */
+export const CartIcon = () => {
+  const { cart } = useCart();
+
+  const [totalItems, setTotalItems] = useState();
+
+  useEffect(() => {
+    setTotalItems(cart.reduce((acc, curr) => acc + Number(curr.quantity), 0));
+  }, [cart]);
+  return (
+    <S.Cart>
+      <CgShoppingBag aria-label="Shopping bag icon" />
+      <S.CartOverlay>{totalItems}</S.CartOverlay>
+    </S.Cart>
+  );
+};
---
+++ src/components/CartIcon/index.styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Cart = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  font-size: 1.5rem;
+`;
+
+export const CartOverlay = styled.div`
+  position: absolute;
+  top: -6px;
+  right: -6px;
+  background-color: ${(props) => props.theme.colors.primary};
+  color: white;
+  font-weight: bold;
+  border-radius: 50%;
+  width: 17px;
+  height: 17px;
+  font-size: 11px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
