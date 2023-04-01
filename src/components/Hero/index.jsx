dbd678e55646197270e747e9e0cfcf3eb581f54a
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-04-01 11:33:19 +0200

    Adds hero section component

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as S from "./index.styles.js";
+
+const Hero = () => {
+  return (
+    <S.HeroImg>
+      <S.HeroHeading>SmartCart</S.HeroHeading>
+      <S.HeroSlogan>Your smartest choice for online shopping</S.HeroSlogan>
+    </S.HeroImg>
+  );
+};
+
+export default Hero;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Hero from "../../components/Hero";
 import ProductsList from "../../components/ProductsList";
 
 export const Home = () => {
   return (
     <main>
+      <Hero />
       <ProductsList />
     </main>
   );
